883871eb433a31ea42438618ab394987b865a8ff
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-05 17:45:33 -0500

    add calculateDecimal and checkBinary

--- src/main/Server.java
+++ src/main/Server.java
@@ -16,6 +16,8 @@ public class Server {
     DataOutputStream out;
 
     final int PORT = 5000;
+    final int OK = 200;
+    final int NOT_BINARY = 400;
 
     try {
       // Created socket on port 5000
@@ -31,11 +33,17 @@ public class Server {
         out = new DataOutputStream(clientSocket.getOutputStream());
 
         // Wait for client message
-        String message = in.readUTF();
-        System.out.println(message);
+        int number = in.readInt();
 
-        // Send message to client
-        out.writeUTF("¡Hello world from Server!");
+        if (checkBinary(number)) {
+          // Send status to client
+          out.writeInt(OK);
+          // Send decimal to client
+          out.writeInt(calculateDecimal(number));
+        } else {
+          // Send status to client
+          out.writeInt(NOT_BINARY);
+        }
 
         // Close client connection
         clientSocket.close();
@@ -47,5 +55,29 @@ public class Server {
       ex.printStackTrace();
     }
 
+  }
+
+  private static int calculateDecimal(int binary) {
+    int decimal = 0;
+    int exponent = 0;
+    int digit;
+    while (binary > 0) {
+      digit = binary % 10;
+      decimal = decimal + digit * (int) Math.pow(2, exponent);
+      binary /= 10;
+      exponent++;
+    }
+    return decimal;
+  }
+
+  private static boolean checkBinary(int num) {
+    int digit;
+    while (num > 0) {
+      digit = num % 10;
+      if (digit != 0 && digit != 1)
+        return false;
+      num /= 10;
+    }
+    return true;
   }
 }
