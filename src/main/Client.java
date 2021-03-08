877cb4af296af89dbca9bde1b00bcadff8396217
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-08 16:27:42 -0500

    completed 🔥

--- src/main/Client.java
+++ src/main/Client.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -12,6 +13,7 @@ public class Client {
     Socket clientSocket = null;
     DataInputStream in;
     DataOutputStream out;
+    Scanner sc;
 
     final String HOST = "127.0.0.1"; // o localhost
     final int PORT = 5000;
@@ -19,19 +21,34 @@ public class Client {
     try {
       // Connect with server
       clientSocket = new Socket(HOST, PORT);
+      System.out.println("\nConnected to the server");
 
+      // initialize data entry
+      sc = new Scanner(System.in);
       in = new DataInputStream(clientSocket.getInputStream());
       out = new DataOutputStream(clientSocket.getOutputStream());
 
-      // Send Message to server
-      out.writeUTF("¡Hello World from client!");
+      // Get ISBN
+      System.out.print("\nEnter ISBN (9 digit): ");
+      String isbnNumber = sc.next();
 
-      // Receive message from server
-      String message = in.readUTF();
-      System.out.println(message);
+      // Send ISBN (9 digit) to Server
+      out.writeUTF(isbnNumber);
+
+      // Receive status from server
+      int status = in.readInt();
+
+      // Assess status
+      if (status == 200) { // all ok
+        String ISBN = in.readUTF();
+        System.out.println("Result: " + ISBN + "\n");
+      } else if (status == 400) { // Bad response
+        System.out.println("Error: number entered is wrong\n");
+      }
 
       // disconnect socket
       clientSocket.close();
+      sc.close();
 
     } catch (IOException ex) {
       System.out.println("\nClient exception: " + ex.getMessage());
--- src/main/Server.java
+++ src/main/Server.java
@@ -16,30 +16,48 @@ public class Server {
     DataOutputStream out;
 
     final int PORT = 5000;
+    final int OK = 200;
+    final int ERROR = 400;
 
     try {
       // Created socket on port 5000
       serverSocket = new ServerSocket(PORT);
-      System.out.println("\nServer listening on port " + PORT);
+      System.out.println("\nServer listening on port " + PORT + "\n");
 
       // listening to clients
       while (true) {
         clientSocket = serverSocket.accept();
-        System.out.println("Client connected");
+        System.out.println("Client connected: " + clientSocket.getLocalAddress());
 
         in = new DataInputStream(clientSocket.getInputStream());
         out = new DataOutputStream(clientSocket.getOutputStream());
 
         // Wait for client message
-        String message = in.readUTF();
-        System.out.println(message);
+        String number = in.readUTF();
+        System.out.println(number.length());
 
-        // Send message to client
-        out.writeUTF("¡Hello world from Server!");
+        if (isISBN(number)) {
+          // Send status to client
+          out.writeInt(OK);
+          // Send ISBN to client
+          out.writeUTF(getCheckDigit(number));
+
+          // Print numbers
+          System.out.println("Status Code: " + OK + " OK");
+          System.out.println("Number Received: " + number);
+          System.out.println("Number Sent: " + getCheckDigit(number));
+        } else {
+          // Send status to client
+          out.writeInt(ERROR);
+
+          // Print Error
+          System.out.println("Status Code: " + ERROR + " ERROR");
+          System.out.println("Number Received: " + number);
+        }
 
         // Close client connection
         clientSocket.close();
-        System.out.println("Client disconnected");
+        System.out.println("Client disconnected\n");
 
       }
     } catch (IOException ex) {
@@ -47,5 +65,38 @@ public class Server {
       ex.printStackTrace();
     }
 
+  }
+
+  public static boolean isISBN(String number) {
+    try {
+      if (number.length() != 9)
+        return false;
+      Integer.parseInt(number);
+      return true;
+    } catch (NumberFormatException nfe) {
+      return false;
+    }
+  }
+
+  public static String getCheckDigit(String number) {
+    // declare variables
+    int sum = 0;
+    int digit = 0;
+    int controlDigit = 0;
+    char ch = '\0';
+
+    // add upto 9th digit
+    for (int i = 1; i <= 9; i++) {
+      ch = number.charAt(i - 1);
+      digit = Character.getNumericValue(ch);
+      sum += (i * digit);
+    }
+
+    if ((sum / 11) < 10) {
+      controlDigit = sum / 11;
+      return number.concat(String.valueOf(controlDigit));
+    }
+
+    return String.valueOf(number.concat(String.valueOf("X")));
   }
 }
