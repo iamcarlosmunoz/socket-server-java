17a6d9a0341a42f379650990522f7f7ee492f0f9
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-26 16:17:09 -0500

    fixed last commit

--- isbn/src/main/Client.java
+++ isbn/src/main/Client.java
@@ -28,38 +28,22 @@ public class Client {
       in = new DataInputStream(clientSocket.getInputStream());
       out = new DataOutputStream(clientSocket.getOutputStream());
 
-<<<<<<< HEAD:isbn/src/main/Client.java
       // Get ISBN
       System.out.print("\nEnter ISBN (9 digit): ");
       String isbnNumber = sc.next();
 
       // Send ISBN (9 digit) to Server
       out.writeUTF(isbnNumber);
-=======
-      // Get number
-      System.out.print("\nEnter binary number: ");
-      int number = sc.nextInt();
-
-      // Send number to Server
-      out.writeInt(number);
->>>>>>> binary:binary/src/main/Client.java
 
       // Receive status from server
       int status = in.readInt();
 
       // Assess status
       if (status == 200) { // all ok
-<<<<<<< HEAD:isbn/src/main/Client.java
         String ISBN = in.readUTF();
         System.out.println("Result: " + ISBN + "\n");
       } else if (status == 400) { // Bad response
         System.out.println("Error: number entered is wrong\n");
-=======
-        int decimal = in.readInt();
-        System.out.println("Decimal: " + decimal + "\n");
-      } else if (status == 400) { // Bad response = not binary
-        System.out.println("Error: Number is NOT binary\n");
->>>>>>> binary:binary/src/main/Client.java
       }
 
       // disconnect socket
